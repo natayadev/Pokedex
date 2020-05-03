12dee23178e18da0c9f43f0aedd299214ee47fca
nataya <natayafs@gmail.com>
2020-05-03 11:38:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import toga
 
+def on_press(widget):
+    print("Holis")
+
+def new_button():
+    button = toga.Button("Hello World", on_press=on_press)
+    button.style.padding = 20
+    button.style.font_size = 14
+    button.style.width = 200
+
+    return button
+
 def build(app):
     box = toga.Box()
+    button1 = new_button()
+    box.add(button1)
     return box
 
 def main():
@@ -11,4 +24,3 @@ def main():
 if __name__ == "__main__":
     app = main()
     app.main_loop()
-    
