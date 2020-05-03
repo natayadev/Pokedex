0bdba7bbd334d42195e9f321523bb8bbd506ce61
nataya <natayafs@gmail.com>
2020-05-03 11:51:24 -0300

    Create const.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import toga
+from const import *
 
 class PokeDex(toga.App):
     def __init__(self, title, id):
         toga.App.__init__(self, title, id)
         self.title = title
-
+        self.size=(WIDTH, HEIGHT)
     def startup(self):
         self.main_window = toga.MainWindow("main", title=self.title,size=(400,500))
         box=toga.Box()
