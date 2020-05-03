94126c4855f5193946a5ed8771a155fa75cf00a0
nataya <natayafs@gmail.com>
2020-05-03 12:37:42 -0300

    Update main.py

---
+++ const.py
@@ -0,0 +1,13 @@
+import os
+
+WIDTH = 600
+HEIGHT = 500
+
+BASE_DIR= os.path.dirname(os.path.abspath(__file__))
+ICON_DIR = os.path.join(BASE_DIR, "icons")
+
+BULBASAUR_ICON = os.path.join(ICON_DIR, "balbasaur.png")
+
+METAPOD_ICON = os.path.join(ICON_DIR, "metapod.png")
+
+PIDGEY_ICON = os.path.join(ICON_DIR, "pidgey.png")
--- main.py
+++ main.py
@@ -1,26 +1,79 @@
 import toga
+import requests
+import threading
 
-def on_press(widget):
-    print("Holis")
+from const import *
 
-def new_button():
-    button = toga.Button("Hello World", on_press=on_press)
-    button.style.padding = 20
-    button.style.font_size = 14
-    button.style.width = 200
+class PokeDex(toga.App):
+    def __init__(self, title, id):
+        toga.App.__init__(self, title, id)
 
-    return button
+        self.title = title
+        self.size=(WIDTH, HEIGHT)
 
-def build(app):
-    box = toga.Box()
-    button1 = new_button()
-    box.add(button1)
-    return box
+        self.heading = ["Name"]
+        self.data = list()
 
-def main():
-    app = toga.App("Hello World", "page", startup=build)
-    return app
+        self.create_elements(self)
+        self.load_async_data()
+
+    def startup(self):
+        self.main_window = toga.MainWindow("main", title=self.title,size=(400,500))
+        box=toga.Box()
+
+        split =  toga.SplitContainer()
+        split.content = [self.table, box]
+
+
+        self.main_window.content = split
+        self.main_window.toolbar.add(self.previous_command, self.next_command)
+        self.main_window.show()
+
+    def create_elements(self):
+        self.create_table()
+        self.create_toolbar()
+
+    def create_toolbar(self):
+        self.create_next_command()
+        self.create_previous_command()
+
+    def create_next_command(self):
+        self.next_command = toga.Command(self.next, label="Next", icon=BULBASAUR_ICON)
+
+    def create_previous_command(self):
+        self.previous_command = toga.Command(self.previous, label="Previous", icon=METAPOD_ICON)
+
+    def create_table(self):
+        self.table = toga.Table(self.heading, data=self.data, on_select=self.select_element)
+
+    def load_async_data(self):
+        thread=threading.Thread(target=self.load_data)
+        thread.start()
+
+    def load_data(self):
+        path="https://pokeapi.co/api/v2/pokemon-form?offset=0&limit=20"
+
+        response = request.get(path)
+        if response:
+            result = responde.json()
+
+            for pokemon in result["results"]:
+                name=pokemon["name"]
+                self.data.append(name)
+
+        self.table.data = self.data
+
+#callbacks
+    def next(self, widget):
+        print("Next")
+
+    def previous(self, widget):
+        print("Previous")
+
+    def select_elements(self, widget, row):
+        if row:
+            print(row.name)
 
 if __name__ == "__main__":
-    app = main()
-    app.main_loop()
+    pokedex = PokeDex("PokeDex", "page")
+    pokedex.main_loop()
