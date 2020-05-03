2271aed1424f33de9966fc7551778a1bff8d4457
nataya <natayafs@gmail.com>
2020-05-03 11:48:53 -0300

    Create app

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import toga
+
+class PokeDex(toga.App):
+    def __init__(self, title, id):
+        toga.App.__init__(self, title, id)
+        self.title = title
+
+    def startup(self):
+        self.main_window = toga.MainWindow("main", title=self.title,size=(400,500))
+        box=toga.Box()
+        self.main_window.content = box
+        self.main_window.show()
+
+    def create_elements(self):
+        pass
+
+#callbacks
+if __name__ == "__main__":
+    pokedex = PokeDex("PokeDex", "page")
+    pokedex.main_loop()
