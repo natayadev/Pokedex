ffce122db43cebd8dc224aa9d494792aa5253697
nataya <natayafs@gmail.com>
2020-05-03 12:11:05 -0300

    Add API

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import toga
+import requests
+
 from const import *
 
 class PokeDex(toga.App):
     def __init__(self, title, id):
         toga.App.__init__(self, title, id)
+
         self.title = title
         self.size=(WIDTH, HEIGHT)
+
         self.heading = ["Name"]
-        self.data = ["Python", "Ruby", "Laravel"]
+        self.data = list()
+
         self.create_elements(self)
+        self.load_data()
 
     def startup(self):
         self.main_window = toga.MainWindow("main", title=self.title,size=(400,500))
@@ -26,6 +32,19 @@ class PokeDex(toga.App):
 
     def create_table(self):
         self.table = toga.Table(self.heading, data=self.data, on_select=self.select_element)
+
+    def load_data(self):
+        path="https://pokeapi.co/api/v2/pokemon-form?offset=0&limit=20"
+
+        response = request.get(path)
+        if response:
+            result = responde.json()
+
+            for pokemon in result["results"]:
+                name=pokemon["name"]
+                self.data.append(name)
+
+        self.table.data = self.data
 
 #callbacks
 def select_elements(self, widget, row):
